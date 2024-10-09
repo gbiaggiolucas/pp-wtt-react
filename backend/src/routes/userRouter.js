47873ab636d75d20b2fcf34e52e1a35b855a149e
Gustavo <gustavobiaggiolucas2007@gmail.com>
2024-10-09 12:19:47 -0300

    swagger

--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -3,7 +3,40 @@ const router = Router();
 
 const { storeUser, login } = require('../controller/userController');
 
+/**
+ * @swagger
+ * /create/account:
+ *  post:
+ *     summary: Cadastra um novo usuário
+ *     responses:
+ *       200:
+ *         description: Sucesso!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                  type object
+ */
+
 router.post('/create/account', storeUser);
+
+/**
+ * @swagger
+ * /auth/login:
+ *  post:
+ *     summary: Verifica um usuario
+ *     responses:
+ *       200:
+ *         description: Sucesso!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                  type object
+ */
+
 router.post('/auth/login', login);
 
 module.exports = router;
